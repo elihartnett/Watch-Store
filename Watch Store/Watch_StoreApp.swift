134b782dc335df17e4e574c5b97833931e489bd9
elichartnett <elichartnett@icloud.com>
2022-08-20 17:45:25 -0500

    Initial Commit

---
+++ Watch Store/Watch_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_StoreApp.swift
+//  Watch Store
+//
+//  Created by Eli Hartnett on 8/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Watch_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
