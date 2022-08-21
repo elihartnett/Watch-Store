fdcb7335c28ef4648ba5cd13d2d170e1f5a4a57e
elichartnett <elichartnett@icloud.com>
2022-08-21 10:36:04 -0500

    Set up constants for assets

---
+++ Watch Store/Constants.swift
@@ -0,0 +1,63 @@
+//
+//  Constants.swift
+//  Watch Store
+//
+//  Created by Eli Hartnett on 8/21/22.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Image {
+        
+        struct Placeholder {
+            static let grid = "grouping_grid"
+            static let list = "grouping_list"
+        }
+        
+        struct Icon {
+            static let apple = "icon_apple"
+            static let bag = "icon_bag"
+            static let cellular = "icon_cellular"
+            static let compatibility = "icon_compatibility"
+            static let heart = "icon_heart"
+            static let productRed = "icon_product_red"
+            static let star = "icon_star"
+            static let wifi = "icon_wifi"
+            static let x = "icon_x"
+        }
+        
+        struct Watch {
+            static let watch1 = "watch1"
+            static let watch2 = "watch2"
+            static let watch3 = "watch3"
+            static let watch4 = "watch4"
+            static let watch5 = "watch5"
+            static let watch6 = "watch6"
+        }
+        
+        struct WatchBand {
+            
+            struct Regular {
+                static let black = "band_regular_black"
+                static let blue = "band_regular_blue"
+                static let brown = "band_regular_brown"
+                static let pink = "band_regular_pink"
+                static let white = "band_regular_white"
+            }
+            
+            struct Solo {
+                static let green = "band_solo_green"
+                static let pink = "band_solo_pink"
+                static let yellow = "band_solo_yellow"
+            }
+            
+            struct Braided {
+                static let black = "band_braided_black"
+                static let blue = "band_braided_blue"
+                static let white = "band_braided_white"
+            }
+        }
+    }
+}
